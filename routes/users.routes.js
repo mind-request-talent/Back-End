2e76545fe23134db950a2e5b3a1edc00ec1953ab
Carlos Rodrigo Acosta Domínguez <71846585+Saf-ire@users.noreply.github.com>
2024-06-03 23:53:50 -0600

    Users CRUD routes

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,8 +3,11 @@ const usersRouter = express.Router();
 
 import * as usersController from '../controllers/users/users.controller.js';
 
-usersRouter.get('/all', usersController.allUsers);
 usersRouter.post('', usersController.create_user);
+usersRouter.get('/all', usersController.allUsers);
+usersRouter.get('/:id', usersController.userById);
+usersRouter.put('/:id', usersController.updateUser);
+usersRouter.delete('/:id', usersController.deleteUser);
 
 
 export default usersRouter;
