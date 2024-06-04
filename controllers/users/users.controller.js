1f3ec6343b5601a47d6b06bd0a7b5fe22320ca8d
Carlos Rodrigo Acosta Domínguez <71846585+Saf-ire@users.noreply.github.com>
2024-06-03 23:50:06 -0600

    Users CRUD

--- controllers/users/users.controller.js
+++ controllers/users/users.controller.js
@@ -2,26 +2,60 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function create_user() {
+export async function create_user(req, res) {
     const { name, permissions } = req.body;
-    await prisma.user.create({
+    const newUser = await prisma.user.create({
         data: {
             name,
             permissions
         },
     });
+
+    if (!newUser) throw new Error('No se pudo crear el usuario')
+
+    return res.json(newUser);
 }
 
 export async function allUsers(req, res) {
-    const users = await prisma.user.findMany();
+    const { skip, take } = req.params
+    if (!skip) skip = 1
+    if (!take) take = 10
+    const users = await prisma.user.findMany({ skip, take });
     if (!users || users.length === 0) return res.status(404).send('No users found');
 
     return res.status(200).json(users);
 
 }
 
+export async function userById(req, res) {
+    const { id } = req.params
+    const user = await prisma.user.findUnique({
+        where: { id }
+    });
+    if (!user) return res.status(404).json(user);
+
+    return res.status(200).json(user);
+}
+
+export async function updateUser(req, res) {
+    const { id } = req.params
+    const { changes } = req.body
+    const updatedUser = await prisma.user.update({
+        where: { id },
+        data: {
+            changes
+        }
+    });
+
+    if (!updatedUser) throw new Error('No se pudo actualizar el usuario')
+
+    return res.status(200).json(updatedUser);
+}
+
 export async function deleteUser(req, res) {
     const { id } = req.params;
-    const user = prisma.user.findUnique(id);
+    const user = prisma.user.findUnique({
+        where: { id }
+    });
     if (!user) return res.send('Ese usuario no existe');
 }
