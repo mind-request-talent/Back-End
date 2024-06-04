b5ef4c2a628e1d18722775a3dc2e9372895c46b0
0Alastor0 <cacosta@arkusnexus.com>
2024-06-04 15:46:55 -0600

    cookie-parser package

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import bodyparser from 'body-parser';
+import cookieParser from 'cookie-parser';
 
 import router from './routes/main.routes.js';
 import usersRouter from './routes/users.routes.js';
 
 const app = express();
+
+app.use(cookieParser());
 app.use(bodyparser.json());
+
 app.use('', router);
 app.use('/users/', usersRouter);
 
